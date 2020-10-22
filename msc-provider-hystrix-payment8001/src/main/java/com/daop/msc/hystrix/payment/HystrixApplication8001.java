f1c3edb6e9d187037b63f9c2ddc80df27a985a80
Gadmind <daopudu@sina.com>
2020-10-22 16:59:01 +0800

    添加bean让服务能通过Hystrix监控

--- msc-provider-hystrix-payment8001/src/main/java/com/daop/msc/hystrix/payment/HystrixApplication8001.java
+++ msc-provider-hystrix-payment8001/src/main/java/com/daop/msc/hystrix/payment/HystrixApplication8001.java
@@ -1,9 +1,12 @@
 package com.daop.msc.hystrix.payment;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @BelongsProject: springboot-cloud
@@ -17,6 +20,22 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableCircuitBreaker
 public class HystrixApplication8001 {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixApplication8001.class,args);
+        SpringApplication.run(HystrixApplication8001.class, args);
+    }
+
+    /**
+     * 此配置是为了服务监控而配置，因为SpringCloud升级后
+     * ServletRegistrationBean中的springboot默认路径不是“/hystrix.stream”
+     * 只要在项目配置下面的Servlet就可以了
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
